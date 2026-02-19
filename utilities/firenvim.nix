fb969d1e336ece8666a987e044715193e693929d
itscrystalline <pvpthadgaming@gmail.com>
2026-02-19 14:47:42 +0700

    update: update firenvim

--- utilities/firenvim.nix
+++ utilities/firenvim.nix
@@ -30,8 +30,10 @@
       "https://nc.iw2tryhard.dev"
       "https://chatgpt.com"
       "https://classroom.google.com"
+      "https://google.com"
       "https://app.slack.com"
       "https://deepl.com"
+      "https://claude.ai"
     ];
 in {
   vim.extraPlugins.firenvim = lib.mkIf full {
@@ -39,8 +41,8 @@ in {
       src = pkgs.fetchFromGitHub {
         owner = "glacambre";
         repo = "firenvim";
-        rev = "c927486daff6d1eb8a0d61fd9e264bc1bf5f2c36";
-        hash = "sha256-TrY86Pd7vN3gEpKNq5FC/QPZR1uMfxz/vvuLuOmFSgU=";
+        rev = "a18ef908ac06b52ad9333b70e3e630b0a56ecb3d";
+        hash = "sha256-3uzx6zJqvNhyqHTwuE108BztSb7WB/2w5EpzvTyTHyU=";
       };
     };
     setup = ''
