0fc8a835fbfc2377d04f196ee80b94bd52528d54
itscrystalline <pvpthadgaming@gmail.com>
2026-05-09 13:19:35 +0700

    csharp: update and add roslyn-nvim plugin

--- languages/csharp.nix
+++ languages/csharp.nix
@@ -1,6 +1,8 @@
 {full, ...}: {
   vim.languages.csharp = {
     enable = full;
-    lsp.servers = ["roslyn_ls"];
+    lsp.servers = ["roslyn-ls"];
+
+    extensions.roslyn-nvim.enable = true;
   };
 }
