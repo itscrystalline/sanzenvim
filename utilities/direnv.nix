0bcfa4c4f2290d9349924658332e24d5e547ac89
itscrystalline <pvpthadgaming@gmail.com>
2026-05-14 20:19:09 +0700

    direnv: disable trick

--- utilities/direnv.nix
+++ utilities/direnv.nix
@@ -1,34 +1,37 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   vim = {
     utility.direnv.enable = true;
     extraPackages = [pkgs.direnv];
-    autocmds = [
-      {
-        enable = true;
-        callback =
-          lib.mkLuaInline
-          # lua
-          ''
-            function()
-              local next = next
-              local clients = vim.lsp.get_clients()
-
-              if next(clients) ~= nil then
-                for _, c in ipairs(clients) do
-                  c:stop()
-                end
-                vim.cmd([[edit]])
-              end
-            end
-          '';
-        desc = "Restart LSPs when direnv has loaded.";
-        event = ["User"];
-        pattern = ["DirenvLoaded"];
-      }
-    ];
+    # causes way more problems than it's worth
+    # autocmds = [
+    #   {
+    #     enable = true;
+    #     callback =
+    #       lib.mkLuaInline
+    #       # lua
+    #       ''
+    #         function()
+    #           local next = next
+    #           local clients = vim.lsp.get_clients()
+    #           local cwd = vim.fn.getcwd()
+    #           local buf_name = vim.api.nvim_buf_get_name(0)
+    #
+    #           if next(clients) ~= nil and buf_name ~= "" then
+    #             for _, c in ipairs(clients) do
+    #               if c.root_dir ~= nil and string.find(c.root_dir, cwd) then
+    #                 c:stop()
+    #               end
+    #             end
+    #             -- if string.find(cwd, buf_name) then
+    #             vim.cmd([[edit]])
+    #             -- end
+    #           end
+    #         end
+    #       '';
+    #     desc = "Restart LSPs when direnv has loaded.";
+    #     event = ["User"];
+    #     pattern = ["DirenvLoaded"];
+    #   }
+    # ];
   };
 }
