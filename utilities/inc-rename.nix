6acad26ae5bf5986cccfa3854cc708f0a6591b73
itscrystalline <pvpthadgaming@gmail.com>
2026-05-15 21:42:38 +0700

    inc-rename: add keybind

--- utilities/inc-rename.nix
+++ utilities/inc-rename.nix
@@ -3,5 +3,21 @@
     package = nur.repos.itscrystalline.inc-rename-nvim;
     setupModule = "inc_rename";
     setupOpts = {};
+    keys = [
+      {
+        mode = "n";
+        key = "cr";
+        action =
+          # lua
+          ''
+            function()
+              return ":IncRename " .. vim.fn.expand("<cword>")
+            end
+          '';
+        lua = true;
+        expr = true;
+        desc = "LSP Rename";
+      }
+    ];
   };
 }
