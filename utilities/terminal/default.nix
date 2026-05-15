c32f13c4c551741b386e8f008ec698d167dac5e0
itscrystalline <pvpthadgaming@gmail.com>
2026-05-15 21:33:16 +0700

    utilities/terminal: make it smarter

--- utilities/terminal/default.nix
+++ utilities/terminal/default.nix
@@ -1,12 +1,51 @@
-_: {
+{lib, ...}: let
+  inherit (lib.nvim.dag) entryBefore;
+in {
   imports = [
     ./lazygit.nix
   ];
+  vim.luaConfigRC.zellij-terminal =
+    entryBefore ["mappings"]
+    # lua
+    ''
+      function _G.ZellijTerminal()
+      	---@type nil|string
+      	_G.opened_zellij_floating = _G.opened_zellij_floating or nil
+
+        local fidget = require("fidget")
+
+      	if _G.opened_zellij_floating ~= nil then
+      		fidget.notify("opening term " .. _G.opened_zellij_floating)
+      		local result = vim.system({ "zellij", "action", "focus-pane-id", _G.opened_zellij_floating }):wait()
+      		if result.code ~= 0 then
+      			fidget.notify(result.stderr, vim.log.levels.WARN)
+      			_G.opened_zellij_floating = nil
+      			_G.ZellijTerminal()
+      		end
+      	else
+      		fidget.notify("opening new term")
+      		local result = vim.system({ "zellij", "action", "new-pane", "-f", "--width", "80%", "--height", "60%", "--cwd", vim.fn.getcwd() }):wait()
+      		if result.code ~= 0 then
+      			fidget.notify("Unable to open new pane. Are you in zellij?", vim.log.levels.ERROR)
+      		else
+      			_G.opened_zellij_floating = result.stdout:gsub("%s+", "")
+      			fidget.notify("got term " .. _G.opened_zellij_floating)
+      		end
+      	end
+      end
+    '';
   vim.keymaps = [
     {
       mode = "n";
       key = "<leader>n";
-      action = '':silent! !zellij action new-pane -f --width 80\% --height 60\%<CR>'';
+      action =
+        # lua
+        ''
+          function()
+            _G:ZellijTerminal()
+          end
+        '';
+      lua = true;
       silent = true;
       desc = "Toggle zellij terminal";
     }
